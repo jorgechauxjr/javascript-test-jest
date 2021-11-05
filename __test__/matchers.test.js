580bfa1958642d8a0616ddb90ec613c6e6cbb12f
Jorge <chauxjorge@hotmail.com>
2021-11-05 17:50:23 -0500

    Add test using matchers .toEqual and .not

---
+++ __test__/matchers.test.js
@@ -0,0 +1,19 @@
+describe('Suite de comparadores comunes/Common Matchers', () => {
+  const user = {
+    name: "juan",
+    lastname: "perez"
+  }
+  const user2 = {
+    name: "juan",
+    lastname: "perez"
+  }
+
+  test('Igualdad de elementos', () => {
+    expect(user).toEqual(user2)
+  });
+
+  test('No son exactamente iguales', () => {
+    expect(user).not.toEqual(user2);
+  });
+
+});
